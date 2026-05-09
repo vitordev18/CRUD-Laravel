93dcf97cec4a35c730eb55434b76cb61ef7cb8bb
vitordev18 <vrferreira18@htomail.com>
2026-05-09 11:51:57 -0300

    Creates functions in the Controller project

--- app/Http/Controllers/Admin/CursoController.php
+++ app/Http/Controllers/Admin/CursoController.php
@@ -4,8 +4,30 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Curso;
 
 class CursoController extends Controller
 {
-    //
+    public function index()
+    {
+        $rows = Curso::all();
+        return view('admin.cursos.index', compact('rows'));
+    }
+
+    public function adicionar()
+    {
+        return view('admin.cursos.adicionar');
+    }
+
+    public function editar($id)
+    {
+        $linha = Curso::find($id);
+        return view('admin.cursos.editar', compact('linha'));
+    }
+
+    public function excluir($id)
+    {
+        Curso::find($id)->delete();
+        return redirect()->route('admin.cursos');
+    }
 }
