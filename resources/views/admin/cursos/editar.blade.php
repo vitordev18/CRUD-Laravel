274d1b29971f4821b47b0059e8127ff8ba6b724b
vitordev18 <vrferreira18@htomail.com>
2026-05-09 11:56:28 -0300

    Create an admin view to manage courses

---
+++ resources/views/admin/cursos/editar.blade.php
@@ -0,0 +1,17 @@
+@extends('layout.site')
+@section('titulo', 'Cursos')
+@section('conteudo')
+    <div class="container">
+        <h3 class="center">Editando Cursos</h3>
+        <div class="row">
+            <form class="" action="{{ route('admin.cursos.atualizar', $linha_id) }}" method="post"
+                  enctype="multipart/form-data">
+                {{ csrf_field() }}
+                <input type="hidden" name="_method" value="put">
+                @include('admin.cursos._form')
+                <button class="btn deep-orange">Atualizar</button>
+            </form>
+        </div>
+    </div>
+
+@endsection
